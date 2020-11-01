8484a3e51f7a68da9dc4220e2be641a395834a04
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2020-11-01 18:06:16 -0500

    Moved Some Code

--- snowStorm.playground/Contents.swift
+++ snowStorm.playground/Contents.swift
@@ -2,19 +2,17 @@ import Foundation
 import CreateML
 import PlaygroundSupport
 
-if let path = Bundle.main.url(forResource: "finalJsonData2", withExtension: "json") {
+if let path = Bundle.main.url(forResource: "finalJsonData", withExtension: "json") {
     do {
         var table = try MLDataTable(contentsOf: path)
         table.removeColumn(named: "date")
-//        print(table.dropMissing().playgroundDescription)
+        print(table.dropMissing().playgroundDescription)
         let split = table.dropMissing().randomSplit(by: 1.0)
-//        var classifier = try MLClassifier(trainingData: split.0, targetColumn: "result")
-//        var boostedcClassifier = try MLBoostedTreeClassifier.init(trainingData: split.0, targetColumn: "result", featureColumns: nil, parameters: MLBoostedTreeClassifier.ModelParameters(validationData: nil, maxDepth: 8, maxIterations: 25, minLossReduction: 0, minChildWeight: 0.1, randomSeed: 42, stepSize: 0.3, earlyStoppingRounds: nil, rowSubsample: 1.0, columnSubsample: 1.0))
-        var c = try MLLogisticRegressionClassifier(trainingData: split.0, targetColumn: "result")
+        let c = try MLLogisticRegressionClassifier(trainingData: split.0, targetColumn: "result")
         
         print("eval")
-//        classifier.evaluation(on: split.1)
-//        boostedcClassifier.evaluation(on: split.1)
+        c.evaluation(on: split.1)
+        c.evaluation(on: split.1)
         let p = playgroundSharedDataDirectory.appendingPathComponent("blizzard2.mlmodel")
         try c.write(to: p, metadata: MLModelMetadata(author: "Addison Hanrattie", shortDescription: "This is the second version of my model that takes in hourly data only from one season", license: "This model may not be shared, used or recreated at all", version: "2.0", additional: nil))
     } catch {
@@ -22,6 +20,8 @@ if let path = Bundle.main.url(forResource: "finalJsonData2", withExtension: "jso
     }
 }
 
+//        var classifier = try MLClassifier(trainingData: split.0, targetColumn: "result")
+//        var boostedcClassifier = try MLBoostedTreeClassifier.init(trainingData: split.0, targetColumn: "result", featureColumns: nil, parameters: MLBoostedTreeClassifier.ModelParameters(validationData: nil, maxDepth: 8, maxIterations: 25, minLossReduction: 0, minChildWeight: 0.1, randomSeed: 42, stepSize: 0.3, earlyStoppingRounds: nil, rowSubsample: 1.0, columnSubsample: 1.0))
 
 /*NOTES:
 
